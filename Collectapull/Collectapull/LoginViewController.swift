0a9b55f54981f6576412fb594837d62949ea1cee
Kolten Gotmer <kgotmer@gmail.com>
2026-03-30 15:38:07 -0500

    Login/Account Pages UI

--- Collectapull/Collectapull/LoginViewController.swift
+++ Collectapull/Collectapull/LoginViewController.swift
@@ -7,7 +7,15 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
 
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
