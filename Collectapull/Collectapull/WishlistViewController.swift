586cd06419e5f84992bc4c6fb9bd0b36410e7c03
PoornimaKallam <kallampoornima@gmail.com>
2026-03-30 21:16:51 -0500

    Wishlist viewcontroller objects added

--- Collectapull/Collectapull/WishlistViewController.swift
+++ Collectapull/Collectapull/WishlistViewController.swift
@@ -4,16 +4,26 @@
 //
 //  Created by Dolan,Allison L on 3/28/26.
 //
-
-
 import UIKit
 
 class WishlistViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var createListButton: UIButton!
+    
+    @IBOutlet weak var collectionAButton: UIButton!
+    
+    @IBOutlet weak var collectionBButton: UIButton!
+    
+    @IBOutlet weak var collectionCButton: UIButton!
+    
+    @IBOutlet weak var collectionDButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
+
+
