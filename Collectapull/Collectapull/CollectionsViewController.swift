df330fcda202496e65d935b65e6666cdb50208a6
Wyatt Ingraham <s540693@nwmissouri.edu>
2026-04-09 11:50:52 -0500

    added firebase and packages dependencies

--- Collectapull/Collectapull/CollectionsViewController.swift
+++ Collectapull/Collectapull/CollectionsViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class CollectionsViewController: UIViewController {
-
+    
+    //existing data
+    var collections : [Collection] = []
+    var selectedCollection : Collection?
+    
+    
+    
+    //added data fields
+    var addName : String = ""
+    var addDescription : String = ""
+    var addImage : UIImage? = nil
+    var addYear : String = ""
+    var addValue : String = ""
+    var addPurchasePrice : String = ""
+    var addType = ""
+    
+    
+    @IBOutlet weak var searchBar: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
